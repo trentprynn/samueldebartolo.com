3f611b10a0ab102c5f050d967e7b006df7ac311b
Trent Prynn <trentprynn@gmail.com>
2021-03-25 07:42:38 -0700

    change anchor linking to use project name instead of id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { LazyLoadImageModule } from "ng-lazyload-image";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppComponent } from './app.component';
-import { ProjectListComponent } from './projects/project-list.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from "./app.component";
+import { ProjectListComponent } from "./projects/project-list.component";
+import { RemoveWhiteSpacePipe } from "./shared/pipes/remove-whitespace.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProjectListComponent
-  ],
+  declarations: [AppComponent, ProjectListComponent, RemoveWhiteSpacePipe],
   imports: [
     BrowserModule,
     CommonModule,
     HttpClientModule,
     LazyLoadImageModule,
-    NgbModule
+    NgbModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [RemoveWhiteSpacePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/pipes/remove-whitespace.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "removeWhiteSpace",
+})
+export class RemoveWhiteSpacePipe implements PipeTransform {
+  /*
+   * Removes white space from the given string
+   * Usage:
+   *   value | removeWhiteSpace
+   * Example:
+   *   {{ "Test String 1" | removeWhiteSpace }}
+   *   formats to: TestString1
+   */
+  transform(value: string): string {
+    return value.replace(/\s/g, "");
+  }
+}
