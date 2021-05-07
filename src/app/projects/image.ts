7e653ea4064cb17e60a1bf1cea910a3a4e6e44fb
Trent Prynn <trentprynn@gmail.com>
2021-05-06 18:04:31 -0700

    rename bisbee project

--- src/app/projects/image.ts
+++ src/app/projects/image.ts
@@ -1,11 +1,13 @@
 export interface IImage {
     src: string
+    alt: string
     width: number
     height: number
 }
 
 export class Image implements IImage {
     src: string
+    alt: string
     width: number
     height: number
 }
