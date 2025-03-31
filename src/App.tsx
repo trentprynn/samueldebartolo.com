386d16762d8aa491732c056fc243ffdfc841eb9d
trentprynn <trentprynn@gmail.com>
2025-03-31 18:31:41 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ import {
   Outlet,
   Navigate,
   NavLink,
+  useMatch,
+  useLocation,
 } from "react-router";
 import { AnimatePresence, motion } from "framer-motion";
-import { FaCircle, FaCircleNotch } from "react-icons/fa6";
-import { useState } from "react";
+import {
+  FaSquareFull,
+  FaRegSquareFull,
+  FaCircle,
+  FaCircleNotch,
+} from "react-icons/fa6";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   return (
@@ -18,63 +25,181 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/montrose" element={<MontroseSanctuary />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 function Home() {
+  type SketchImage = {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+  };
+
+  const [currentImage, setCurrentImage] = useState<SketchImage | null>(null);
+  const didMount = useRef(false);
+
+  // List of all available images
+  const sketchImages: SketchImage[] = [
+    {
+      src: "/assets/sketches/brocconi_2268_3024.png",
+      alt: "Brocconi sketch",
+      width: 2268,
+      height: 3024,
+    },
+    {
+      src: "/assets/sketches/catholic_church_2689_2930.png",
+      alt: "Catholic Church sketch",
+      width: 2689,
+      height: 2930,
+    },
+    {
+      src: "/assets/sketches/kunsthaus_zurich_3024_4032.png",
+      alt: "Kunsthaus Zurich sketch",
+      width: 3024,
+      height: 4032,
+    },
+    {
+      src: "/assets/sketches/st_benedict_2269_3024.png",
+      alt: "St Benedict sketch",
+      width: 2269,
+      height: 3024,
+    },
+    {
+      src: "/assets/sketches/therme_vals_998_1331.png",
+      alt: "Therme Vals sketch",
+      width: 998,
+      height: 1331,
+    },
+  ];
+
+  useEffect(() => {
+    // This ensures the effect logic runs only once
+    if (!didMount.current) {
+      didMount.current = true;
+
+      const lastViewed = localStorage.getItem("lastViewedSketch");
+      let availableImages = [...sketchImages];
+
+      if (lastViewed) {
+        availableImages = availableImages.filter(
+          (img) => img.src !== lastViewed,
+        );
+      }
+
+      if (availableImages.length === 0) {
+        availableImages = [...sketchImages];
+      }
+
+      const randomIndex = Math.floor(Math.random() * availableImages.length);
+      const selected = availableImages[randomIndex];
+
+      setCurrentImage(selected);
+      localStorage.setItem("lastViewedSketch", selected.src);
+    }
+  }, []);
+
   return (
-    <div className="flex items-center justify-end h-80">
-      <h1 className="text-2xl">samuel debartolo</h1>
+    <div className="flex flex-col justify-end items-end md:flex-row md:items-start pt-[5rem] gap-6">
+      <div className="order-2 md:order-1">
+        {currentImage && (
+          <LazyImage
+            src={currentImage.src}
+            alt={currentImage.alt}
+            className="object-cover w-[300px] md:w-[400px]"
+            width={currentImage.width}
+            height={currentImage.height}
+          />
+        )}
+      </div>
+      <div className="flex flex-col order-1 pt-24 md:order-2">
+        <h1 className="text-2xl">samuel debartolo</h1>
+      </div>
     </div>
   );
 }
 
-function MontroseSanctuary() {
+function Work() {
   return (
     <div>
       <div>
-        <h1 className="text-2xl font-semibold">montrose sanctuary</h1>
-        <p className="mt-2 text-xs font-thin">october 2023 - present</p>
+        <h1 className="text-2xl font-thin text-end">sojourn montrose</h1>
       </div>
 
-      <div className="flex flex-wrap gap-8 mt-4">
-        <LazyImage
-          src="/assets/montrose/before_3992_3100.jpg"
-          alt="Montrose Sanctuary exterior before renovation"
-          className="object-cover w-48 md:w-72"
-          width={3992}
-          height={3100}
-        />
-        <LazyImage
-          src="/assets/montrose/render_one_3992_3100.jpg"
-          alt="Montrose Sanctuary external render"
-          className="object-cover w-48 md:w-72"
-          width={3992}
-          height={3100}
-        />
-        <LazyImage
-          src="/assets/montrose/render_two_2782_2160.jpg"
-          alt="Montrose Sanctuary interior render"
-          className="object-cover w-48 md:w-72"
-          width={2782}
-          height={2160}
-        />
-      </div>
+      <div className="flex flex-wrap gap-8 mt-4 justify-end">
+        <div className="flex flex-col gap-2">
+          <LazyImage
+            src="/assets/montrose/past_01_2685_2085.png"
+            alt="Montrose Sanctuary exterior before renovation - img one"
+            className="object-cover w-52 md:w-72"
+            width={2685}
+            height={2085}
+          />
+          <LazyImage
+            src="/assets/montrose/past_02_2685_2085.png"
+            alt="Montrose Sanctuary exterior before renovation - img two"
+            className="object-cover w-52 md:w-72"
+            width={2685}
+            height={2085}
+          />
+          <p>past</p>
+        </div>
 
-      <div className="mt-4">
-        <p>
-          A sensitive renovation of a 1960s historic church in the heart of
-          Houston, Texas. The design seeks to distill the essence of sacred
-          worship and foster authentic community connection, all within the
-          quiet discipline of a restrained budget. The project embraces what is
-          essential, allowing the space to breathe and resonate with those who
-          gather, cultivating an atmosphere of reverence and simplicity.
-        </p>
+        <div className="flex flex-col gap-2">
+          <LazyImage
+            src="/assets/montrose/present_01_2685_2085.png"
+            alt="Montrose Sanctuary exterior during renovation - img one"
+            className="object-cover w-52 md:w-72"
+            width={2685}
+            height={2085}
+          />
+          <LazyImage
+            src="/assets/montrose/present_02_2685_2085.png"
+            alt="Montrose Sanctuary exterior during renovation - img two"
+            className="object-cover w-52 md:w-72"
+            width={2685}
+            height={2085}
+          />
+          <p>present</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <LazyImage
+            src="/assets/montrose/future_01_2685_2085.png"
+            alt="Montrose Sanctuary exterior during renovation - img one"
+            className="object-cover w-52 md:w-72"
+            width={2685}
+            height={2085}
+          />
+          <LazyImage
+            src="/assets/montrose/future_02_2685_2085.png"
+            alt="Montrose Sanctuary exterior during renovation - img two"
+            className="object-cover w-52 md:w-72"
+            width={2685}
+            height={2085}
+          />
+          <p>future</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div className="flex flex-col justify-end items-end md:flex-row md:items-start pt-[5rem] gap-6">
+      <div className="order-2 md:order-1"></div>
+      <div className="flex flex-col order-1 pt-24 md:order-2">
+        <h1 className="text-2xl">samuel debartolo</h1>
+        <a href="tel:+16026530533" className="pt-12">
+          602.327.0390
+        </a>
+        <a href="mailto:samueldebartolo@me.com">samueldebartolo@me.com</a>
       </div>
     </div>
   );
@@ -98,7 +223,7 @@ function LazyImage({
   return (
     <div className="relative">
       {loading && (
-        <div className="absolute inset-0 flex items-center justify-center bg-white/70">
+        <div className="absolute inset-0 flex items-center justify-center">
           <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-600" />
         </div>
       )}
@@ -117,16 +242,26 @@ function LazyImage({
 }
 
 function Layout() {
-  return (
-    <div className="overflow-hidden">
-      <div className="flex">
-        <div className="flex-none w-14 h-dvh overflow-hidden">
-          <SideNav />
-        </div>
+  const location = useLocation();
 
-        <div className="flex-1 h-dvh overflow-auto px-5 pt-3 pb-10">
-          <Outlet />
-        </div>
+  return (
+    <div className="relative overflow-hidden bg-[#121212] text-white font-thin min-h-dvh">
+      <div className="absolute left-0 top-0 h-full">
+        <SideNav />
+      </div>
+
+      <div className="h-dvh overflow-auto px-5 pt-3 pb-10 md:ml-40">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            /* No exit => old content is removed instantly */
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );
@@ -134,17 +269,26 @@ function Layout() {
 
 function SideNav() {
   return (
-    <div className="h-full flex flex-col justify-between">
+    <div className="h-full flex flex-col">
       <SideNavEntry
-        href="/montrose"
-        activeIcon={<img src="/assets/montrose/3_windows_icon_587_589.png" />}
-        inactiveIcon={<img src="/assets/montrose/block_587_589.png" />}
+        href="/work"
+        activeIcon={<FaRegSquareFull size={14} />}
+        inactiveIcon={<FaSquareFull size={14} />}
+        text="built work"
       />
-
+      <SideNavEntry
+        href="/contact"
+        activeIcon={<FaRegSquareFull size={14} />}
+        inactiveIcon={<FaSquareFull size={14} />}
+        text="contact"
+      />
+      {/* spacer */}
       <SideNavEntry
         href="/"
-        activeIcon={<FaCircleNotch size={30} />}
-        inactiveIcon={<FaCircle size={30} />}
+        activeIcon={<FaCircleNotch size={14} />}
+        inactiveIcon={<FaCircle size={14} />}
+        className="mt-auto"
+        text="home"
       />
     </div>
   );
@@ -154,46 +298,56 @@ function SideNavEntry({
   href,
   activeIcon,
   inactiveIcon,
+  text,
+  className,
 }: {
   href: string;
   activeIcon: React.ReactNode;
   inactiveIcon: React.ReactNode;
+  text: string;
+  className?: string;
 }) {
-  return (
-    <NavLink
-      to={href}
-      className="relative object-cover w-14 h-14 p-3 overflow-hidden hover:bg-zinc-100 flex items-center justify-center"
-    >
-      {({ isActive }) => (
-        <>
-          <AnimatePresence mode="wait">
-            {isActive ? (
-              <motion.div
-                key="activeIcon"
-                initial={{ opacity: 0, scale: 0.9 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.9 }}
-                transition={{ duration: 0.15 }}
-              >
-                {activeIcon}
-              </motion.div>
-            ) : (
-              <motion.div
-                key="inactiveIcon"
-                initial={{ opacity: 0, scale: 0.9 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.9 }}
-                transition={{ duration: 0.15 }}
-              >
-                {inactiveIcon}
-              </motion.div>
-            )}
-          </AnimatePresence>
+  const isActive = useMatch(href);
 
-          {isActive && <div className="w-1 h-4 bg-black absolute right-0" />}
-        </>
+  return (
+    <div className={`flex items-center ${className}`}>
+      <NavLink
+        to={href}
+        className="w-14 h-14 p-3 flex items-center justify-center relative overflow-hidden hover:bg-neutral-900 transition-all duration-200"
+      >
+        <AnimatePresence mode="wait">
+          {isActive ? (
+            <motion.div
+              key="activeIcon"
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              transition={{ duration: 0.15 }}
+              className="flex-shrink-0"
+            >
+              {activeIcon}
+            </motion.div>
+          ) : (
+            <motion.div
+              key="inactiveIcon"
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              transition={{ duration: 0.15 }}
+              className="flex-shrink-0"
+            >
+              {inactiveIcon}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </NavLink>
+
+      {isActive && (
+        <p className="text-xs">
+          <u>{text}</u>
+        </p>
       )}
-    </NavLink>
+    </div>
   );
 }
 
